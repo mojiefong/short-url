982779cfd09b4f571f89e19ca624604e0ef4867b
Mojie <mojiefong@gmail.com>
2025-11-19 15:33:11 +0800

    chore: 优化代码

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ WORKDIR /app
 ENV PNPM_HOME="/pnpm"
 ENV PATH="$PNPM_HOME:$PATH"
 COPY . .
-RUN corepack enable
 
-CMD [ "node", "--experimental-transform-types", "--no-warnings", "--env-file=.env", "src/main.ts" ]
+# CMD [ "node", "--experimental-transform-types", "--no-warnings", "--env-file=.env", "main.ts" ]
+CMD [ "node", "--experimental-transform-types", "--no-warnings", "src/main.ts" ]
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,16 @@
  * @Date: 2025-05-23 10:25:08
  */
 
+import { readFileSync } from 'node:fs'
+import { join } from 'node:path'
 import process from 'node:process'
 import { loadUrls, watchUrls } from './config/index.ts'
 import { createAppServer } from './server/index.ts'
 
 const PORT = process.env.APP_PORT || 9527
+
+const pkgPath = join(process.cwd(), 'package.json')
+const pkg = JSON.parse(readFileSync(pkgPath, 'utf-8'))
 
 function bootstrap() {
   const urlMap = loadUrls()
@@ -17,7 +22,7 @@ function bootstrap() {
 
   server.listen(PORT, () => {
     console.log(`服务已启动：http://localhost:${PORT}`)
-    console.log(`当前版本：${process.env.npm_package_version}`)
+    console.log(`当前版本：${pkg.version}`)
     console.log(`示例：http://localhost:${PORT}/bd → 跳转百度`)
   })
 }
